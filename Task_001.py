47b9104c3541038a553a06df76fda812c75e82d8
Alina Lavrik <alina_voro@mail.ru>
2022-11-28 22:41:28 +0300

    Add variant_2

--- Task_001.py
+++ Task_001.py
@@ -1,7 +1,6 @@
 # Задайте список. Напишите программу, которая определит, присутствует ли в заданном списке строк некое число.
 
-spisok = ['123', 234 , 'sos', '!!!', '__']
-
+'''spisok = ['123', 234 , 'sos', '!!!', '__']
 n = 0
 for i in spisok:
 
@@ -10,10 +9,17 @@ for i in spisok:
 if n > 0:
     print('Yes') 
 else:
+    print('No')'''
+
+
+# Вариант 2
+
+mass = ['ssss', 'sngujn556', '44', 258]
+types = [str(type(i)) for i in mass]
+if "<class 'int'>" in types or "<class 'float'>" in types:
+    print('Yes')
+else:
     print('No')
-
-
-
 
 
 
